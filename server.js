f65e366835e46603e522e902ebd8ff8c70676350
aspiringguru <bmatthewtaylor@gmail.com>
2019-06-10 17:30:04 +1000

    changed sqlite database from memory to file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(':memory:', (err) => {
+//let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
   if (err) {
     return console.error(err.message);
   }
